a6d1c9d7572902437bfcdd7a8d51557632d804d4
SethDonohue <donohue.seth@gmail.com>
2018-05-17 16:28:30 -0700

    cleaned up console logs

--- src/grid/grid.js
+++ src/grid/grid.js
@@ -12,43 +12,26 @@ class Grid extends Component {
       const randomIndex = this.randomInt(this.props.colorSet.length);
       map[(this.props.colorSet[randomIndex])] = 2;
     }
-
+    // add arr oh keys onto self to double array length
     const mapKeys = Object.keys(map);
-    
-    // add arr onto self to double array length
     const allColorsArr = mapKeys.concat(mapKeys);
 
     // loop through arr for length and randomly insert elements from that arr into a new tempArr
     const tempArr = [];
     while (allColorsArr.length > 0) {
-      // if(map[])
       const randomIndex = this.randomInt(allColorsArr.length);
       tempArr.push(allColorsArr[randomIndex]);
       allColorsArr.splice(randomIndex, 1);
-      // console.log('Sliced allColorsArr: ', allColorsArr);
     }
-
     return tempArr;
   }
 
-  // pickRandomColor = (map) => {
-  //   const max = Object.keys(map).length - 1;
-  //   const randomColor = Object.keys(map)[this.randomInt(max)];
-  //   console.log('Random Color: ', randomColor);
-  //   return randomColor;
-  // }
-
   createGrid = () => {
     const colorArr = this.randomColorArr();
-    console.log('colorArr: ', colorArr);
 
-    // pull 1 color from colorArr to put into 1 Card
     const pickColor = () => {
-      const color = colorArr.pop();
-      console.log('color Picked: ', color);
-      return color;
+      return colorArr.pop();
     };
-
 
     const grid = [];
 
