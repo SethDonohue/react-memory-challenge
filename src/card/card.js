695bb741fc3d53438302e72eb207b48a832b7f17
SethDonohue <donohue.seth@gmail.com>
2018-05-21 12:36:44 -0700

    changed card from div to button to allow disable

--- src/card/card.js
+++ src/card/card.js
@@ -2,41 +2,14 @@ import React, { Component } from 'react';
 import './card.css';
 
 class Card extends Component {
-  state = {
-    // flipped: false,
-    cardColor: 'gray',
-    cardText: 'Back',
-  }
-
-  // handleClick = event => {
-  //   if (this.state.cardColor === 'gray') {
-  //     // set the div bg color attr to the color
-  //     this.setState({
-  //       cardColor: this.props.cardColor,
-  //       cardText: 'Front',
-  //     });
-  //   } else {
-  //     // set the div bg color attr to the gray
-  //     this.setState({
-  //       cardColor: 'gray',
-  //       cardText: 'Back',
-  //     });
-  //   }
-  // }
-
-  
   render() {
     return (
-      /* eslint-disable */
-      <div
+      <button
         className="card"
-      // key={this.props.key}
-      // onClick={this.handleClick}
-        style={{ backgroundColor: this.state.cardColor }}
+        style={{ backgroundColor: this.props.cardColor }}
       >
-        <span>{this.state.cardText}</span>
-      </div>
-      /* eslint-enable */
+        Card
+      </button>
     );
   }
 }
--- src/grid/grid.js
+++ src/grid/grid.js
@@ -3,66 +3,20 @@ import './grid.css';
 import Card from '../card/card';
 
 class Grid extends Component {
-  // componentDidMount() {
-  //   console.log('Grid Mounted');
-  //   // Run createGrid() here so it only happens once, not everytme state is updated..
-  //   // const grid = this.createGrid();
-  //   this.setState({ //eslint-disable-line
-  //     grid: this.randomColorArr(),
-  //   });
-  // }
+  handleclick = (event) => {
+    const card = event.target;
 
-  // randomInt = (max) => Math.floor(Math.random() * Math.floor(max));
+    if(card.style.backgroundColor === 'gray'){
 
-  // randomColorArr = () => {
-  //   // create 8 random colors in a map with keys
-  //   const map = {};
-  //   while (Object.keys(map).length < 8) {
-  //     const randomIndex = this.randomInt(this.props.colorSet.length);
-  //     map[(this.props.colorSet[randomIndex])] = 2;
-  //   }
-  //   // add arr oh keys onto self to double array length
-  //   const mapKeys = Object.keys(map);
-  //   const allColorsArr = mapKeys.concat(mapKeys);
-
-  //   // loop through arr for length and randomly insert elements from that arr into a new tempArr
-  //   const tempArr = [];
-  //   while (allColorsArr.length > 0) {
-  //     const randomIndex = this.randomInt(allColorsArr.length);
-  //     tempArr.push(allColorsArr[randomIndex]);
-  //     allColorsArr.splice(randomIndex, 1);
-  //   }
-  //   return tempArr;
-  // }
-
-  // createGrid = () => {
-  //   const colorArr = this.randomColorArr();
-
-  //   const pickColor = () => {
-  //     return colorArr.pop();
-  //   };
-
-  //   const grid = [];
-
-  //   // Outerloop for tr
-  //   for (let i = 0; i < 4; i++) {
-  //     const rows = [];
-  //     // Inner for td
-  //     for (let j = 0; j < 4; j++) {
-  //       rows.push(<td key={(j * 10) + i}><Card cardColor={pickColor()} /></td>);
-  //     }
-  //     grid.push(<tr key={(i * 5)} >{rows}</tr>);
-  //   }
-  //   return grid;
-  // }
-
+    }
+  }
   render() {
     return (
       <table id="grid">
         <tbody>
           <tr>
-            <td><Card cardColor={this.props.colorArr[0]} /></td><td><Card cardColor={this.props.colorArr[1]} /></td>
-            <td><Card cardColor={this.props.colorArr[2]} /></td><td><Card cardColor={this.props.colorArr[3]} /></td>
+            <td><Card key={11} onClick={this.handleclick} cardColor={this.props.colorArr[0]} /></td><td><Card key={12} cardColor={this.props.colorArr[1]} /></td>
+            <td><Card key={13} cardColor={this.props.colorArr[2]} /></td><td><Card key={14} cardColor={this.props.colorArr[3]} /></td>
           </tr>
           <tr>
             <td><Card cardColor={this.props.colorArr[4]} /></td><td><Card cardColor={this.props.colorArr[5]} /></td>
