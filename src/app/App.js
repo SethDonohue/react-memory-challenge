8ad03ec24ab66b6dfd5f9b5cee1e7cc129dfb59e
SethDonohue <donohue.seth@gmail.com>
2018-05-21 12:00:31 -0700

    grid rendering with gray and props with random color pairs

--- src/app/App.js
+++ src/app/App.js
@@ -14,12 +14,47 @@ class App extends Component {
       'rgb(300, 0, 0)', 'rgb(0, 300, 0)', 'rgb(0, 0, 300)',
       'rgb(300, 300, 0)',
     ],
-
     // 'rgb(0, 300, 300)', 'rgb(300, 0, 300)',
     // 'rgb(100, 100, 100)', 'rgb(0, 0, 0)', 'rgb(200, 200, 200)',
     // 'rgb(256, 256, 256)'
   }
+
+  // componentDidMount() {
+  //   console.log('Grid Mounted');
+  //   // Run createGrid() here so it only happens once, not everytme state is updated..
+  //   // const grid = this.createGrid();
+  //   this.setState({ //eslint-disable-line
+  //     colorArr: this.randomColorArr(),
+  //   });
+  // }
+
+
+  randomInt = (max) => Math.floor(Math.random() * Math.floor(max));
+
+  randomColorArr = () => {
+    // create 8 random colors in a map with keys
+    const map = {};
+    while (Object.keys(map).length < 8) {
+      const randomIndex = this.randomInt(this.state.colorSet.length);
+      map[(this.state.colorSet[randomIndex])] = 2;
+    }
+    // add arr oh keys onto self to double array length
+    const mapKeys = Object.keys(map);
+    const allColorsArr = mapKeys.concat(mapKeys);
+
+    // loop through arr for length and randomly insert elements from that arr into a new tempArr
+    const tempArr = [];
+    while (allColorsArr.length > 0) {
+      const randomIndex = this.randomInt(allColorsArr.length);
+      tempArr.push(allColorsArr[randomIndex]);
+      allColorsArr.splice(randomIndex, 1);
+    }
+    return tempArr;
+  }
+
   render() {
+    const colorArr = this.randomColorArr();
+
     return (
       <div className="App">
         <header className="App-header">
@@ -28,7 +63,7 @@ class App extends Component {
         </header>
         <Grid
           cardAmount={this.state.cardAmount}
-          colorSet={this.state.colorSet}
+          colorArr={colorArr}
         />
       </div>
     );
--- src/grid/grid.js
+++ src/grid/grid.js
@@ -3,28 +3,37 @@ import './grid.css';
 import Card from '../card/card';
 
 class Grid extends Component {
-  randomInt = (max) => Math.floor(Math.random() * Math.floor(max));
+  // componentDidMount() {
+  //   console.log('Grid Mounted');
+  //   // Run createGrid() here so it only happens once, not everytme state is updated..
+  //   // const grid = this.createGrid();
+  //   this.setState({ //eslint-disable-line
+  //     grid: this.randomColorArr(),
+  //   });
+  // }
 
-  randomColorArr = () => {
-    // create 8 random colors in a map with keys
-    const map = {};
-    while (Object.keys(map).length < 8) {
-      const randomIndex = this.randomInt(this.props.colorSet.length);
-      map[(this.props.colorSet[randomIndex])] = 2;
-    }
-    // add arr oh keys onto self to double array length
-    const mapKeys = Object.keys(map);
-    const allColorsArr = mapKeys.concat(mapKeys);
+  // randomInt = (max) => Math.floor(Math.random() * Math.floor(max));
 
-    // loop through arr for length and randomly insert elements from that arr into a new tempArr
-    const tempArr = [];
-    while (allColorsArr.length > 0) {
-      const randomIndex = this.randomInt(allColorsArr.length);
-      tempArr.push(allColorsArr[randomIndex]);
-      allColorsArr.splice(randomIndex, 1);
-    }
-    return tempArr;
-  }
+  // randomColorArr = () => {
+  //   // create 8 random colors in a map with keys
+  //   const map = {};
+  //   while (Object.keys(map).length < 8) {
+  //     const randomIndex = this.randomInt(this.props.colorSet.length);
+  //     map[(this.props.colorSet[randomIndex])] = 2;
+  //   }
+  //   // add arr oh keys onto self to double array length
+  //   const mapKeys = Object.keys(map);
+  //   const allColorsArr = mapKeys.concat(mapKeys);
+
+  //   // loop through arr for length and randomly insert elements from that arr into a new tempArr
+  //   const tempArr = [];
+  //   while (allColorsArr.length > 0) {
+  //     const randomIndex = this.randomInt(allColorsArr.length);
+  //     tempArr.push(allColorsArr[randomIndex]);
+  //     allColorsArr.splice(randomIndex, 1);
+  //   }
+  //   return tempArr;
+  // }
 
   // createGrid = () => {
   //   const colorArr = this.randomColorArr();
@@ -51,7 +60,22 @@ class Grid extends Component {
     return (
       <table id="grid">
         <tbody>
-          <tr><td><Card cardColor={this.} /></td><td>Card 1</td><td>Card 1</td><td>Card 1</td></tr>
+          <tr>
+            <td><Card cardColor={this.props.colorArr[0]} /></td><td><Card cardColor={this.props.colorArr[1]} /></td>
+            <td><Card cardColor={this.props.colorArr[2]} /></td><td><Card cardColor={this.props.colorArr[3]} /></td>
+          </tr>
+          <tr>
+            <td><Card cardColor={this.props.colorArr[4]} /></td><td><Card cardColor={this.props.colorArr[5]} /></td>
+            <td><Card cardColor={this.props.colorArr[6]} /></td><td><Card cardColor={this.props.colorArr[7]} /></td>
+          </tr>
+          <tr>
+            <td><Card cardColor={this.props.colorArr[8]} /></td><td><Card cardColor={this.props.colorArr[9]} /></td>
+            <td><Card cardColor={this.props.colorArr[10]} /></td><td><Card cardColor={this.props.colorArr[11]} /></td>
+          </tr>
+          <tr>
+            <td><Card cardColor={this.props.colorArr[12]} /></td><td><Card cardColor={this.props.colorArr[13]} /></td>
+            <td><Card cardColor={this.props.colorArr[14]} /></td><td><Card cardColor={this.props.colorArr[15]} /></td>
+          </tr>
         </tbody>
       </table>
     );
